9d71ee3f3756325025d5b2f13eecc437b6b49748
Naïm BOUGHATTAS <boughattas.naim@gmail.com>
2024-11-27 22:52:38 +0100

    Test update API

--- apps/api/app.js
+++ apps/api/app.js
@@ -13,14 +13,14 @@ admin.initializeApp({
 });
 
 const firestoreApp = initializeApp({
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   databaseURL:
     "https://social-marketplace-9eb85-default-rtdb.europe-west1.firebasedatabase.app/",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
 });
 
 // Obtention de la référence à la base de données
