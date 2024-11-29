cd1ea215824f28efdd8810dfb030b9feadd3b8b6
Naïm BOUGHATTAS <boughattas.naim@gmail.com>
2024-11-29 16:01:40 +0100

    Test update API

--- apps/api/controllers/linkedin.js
+++ apps/api/controllers/linkedin.js
@@ -1,3 +1,7 @@
+import { db } from "../firebase";
+const { collection, addDoc, Timestamp } = require("firebase/firestore");
+const { getCachedData } = require("../redis");
+
 const CLIENT_ID = "780d3m61bi94hx";
 const CLIENT_SECRET = "WPL_AP1.TgARA6RNgPla8T4z.vwO3/A==";
 
@@ -11,13 +15,29 @@ export const generateAuthURL = (req, res) => {
 export const OAuthCallback = async (req, res) => {
   const code = req.query.code; // Code d'autorisation reçu après l'authentification
   const userId = req.query.state; // userId passé dans le paramètre 'state'
-  const { access_token } = await getAccessToken(code);
-  const userInfo = await getUserInfo(access_token);
-  res.send({
-    code,
-    userId,
-    userInfo,
-  });
+  try {
+    const { access_token } = await getAccessToken(code);
+    console.log("Token:", access_token);
+    const pageData = await getUserInfo(access_token);
+    console.log("pageData:", pageData);
+    const formData = await getCachedData(userId);
+    console.log("formData:", formData);
+    const accountRef = await addDoc(collection(db, "socialAccounts"), {
+      userId,
+      ...formData,
+      ...pageData,
+      createdAt: Timestamp.now(),
+    });
+    res.redirect(
+      "https://the-reach-market-dashboard.vercel.app/dashboard/my-accounts"
+    );
+  } catch (error) {
+    console.error("Error while retrieving access token from Twitter:", error);
+    res.status(500).json({
+      error: "Failed to obtain access token",
+      details: error.message,
+    });
+  }
 };
 
 export const getAccessToken = async (code) => {
